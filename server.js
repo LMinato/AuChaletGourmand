ba739ea2cd61bcfdf05008eb4b0966bf6835c0ea
LMinato <lcorailler@gmail.com>
2019-01-29 22:46:57 +0100

    WIP MEAN

--- server.js
+++ server.js
@@ -1,17 +1,71 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-const app = express();
-const nameApp = 'AuChaletGourmand';
+var CONTACTS_COLLECTION = "contacts";
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/' + nameApp));
+var app = express();
+app.use(bodyParser.json());
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/'+ nameApp +'/index.html'));
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+  }
+  
+  /*  "/api/contacts"
+   *    GET: finds all contacts
+   *    POST: creates a new contact
+   */
+  
+  app.get("/api/contacts", function(req, res) {
+    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+        if (err) {
+          handleError(res, err.message, "Failed to get contacts.");
+        } else {
+          res.status(200).json(docs);
+        }
+      });
+  });
+  
+  app.post("/api/contacts", function(req, res) {
+  });
+  
+  /*  "/api/contacts/:id"
+   *    GET: find contact by id
+   *    PUT: update contact by id
+   *    DELETE: deletes contact by id
+   */
+  
+  app.get("/api/contacts/:id", function(req, res) {
+  });
+  
+  app.put("/api/contacts/:id", function(req, res) {
+  });
+  
+  app.delete("/api/contacts/:id", function(req, res) {
+  });
