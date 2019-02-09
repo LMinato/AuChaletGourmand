daddfc67e5e76390011f126ca669992fe769f15b
LMinato <lcorailler@gmail.com>
2019-02-09 14:41:02 +0100

    Update under contruction title

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/AuChaletGourmand'));
+//app.use(express.static(__dirname + '/dist/AuChaletGourmand'));
+app.use(express.static(__dirname + '/UnderConstruction'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/AuChaletGourmand/index.html'));
+app.get('/', function(req,res) {
+    res.redirect('/underconstruction');
+    //res.sendFile(path.join(__dirname+'/dist/AuChaletGourmand/index.html'));
 });
+app.get('/underconstruction', function(req, res) {
+    res.sendFile(path.join(__dirname + '/UnderConstruction/index.html'))
+})
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
